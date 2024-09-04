a64f11fb6fe90d7b4863af76380f307bb2392780
Mbongeni.Christian <mbongenichristian3@gmail.com>
2024-09-04 18:08:42 +0200

    Hello

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-database_url = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://taskify_itvz_user:OE3JJaiIS7MrFTibWD5eTo2twtqq7czj@dpg-cra5rvaj1k6c73bs5qag-a/taskify_itv')
 
-# Replace 'postgres://' with 'postgresql://'
-if database_url and database_url.startswith('postgres://'):
-    database_url = database_url.replace('postgres://', 'postgresql://', 1)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
